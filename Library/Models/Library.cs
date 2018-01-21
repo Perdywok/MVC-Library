110e35d758286ca2850c82de45796e24df36fdac
Влад <xset96@gmail.com>
2018-01-21 21:16:11 +0200

    first commit

---
+++ Library/Models/Library.cs
@@ -0,0 +1,31 @@
+namespace Library.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Library : DbContext
+    {
+        // Your context has been configured to use a 'Book' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Library.Models.Book' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'Book' 
+        // connection string in the application configuration file.
+        public Library()
+            : base("name=Library")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
